e7f6756981989beffcd985b648fa855d48fd5ec7
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-07 14:59:45 +0530

    n = int(input()) List = list(map(int,input().split())) num = 0 for i in range(0,n):     num += List[i]*(2**(n - i - 1)) print(num)

--- Binary_Array_to_decimal.py
+++ Binary_Array_to_decimal.py
@@ -2,6 +2,5 @@ n = int(input())
 List = list(map(int,input().split()))
 num = 0
 for i in range(0,n):
-    if List[i] == 1:
-        num += (2**(n - i - 1))
+    num += List[i]*(2**(n - i - 1))
 print(num)
