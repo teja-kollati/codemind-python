cadea49dd7cc15df6c806594cacd4e121a5dce3b
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-11 18:36:25 +0530

    def count(List,n):     c = 0     for i in List:         if i == n:             c += 1     return c; n = int(input()) List = list(map(int,input().split())) l = [] for i in List:     if count(List,i) == i:         l.append(i) if len(l) == 0:     print(-1) else:     print("{:.2f}".format(sum(set(l))/len(set(l))))

--- Average_of_super_elements.py
+++ Average_of_super_elements.py
@@ -2,15 +2,15 @@ def count(List,n):
     c = 0
     for i in List:
         if i == n:
-            c += 1;
-    return c
+            c += 1
+    return c;
 n = int(input())
 List = list(map(int,input().split()))
 l = []
 for i in List:
     if count(List,i) == i:
         l.append(i)
-if len(l) != 0:
-    print("{:.2f}".format(sum(set(l))/len(set(l))))
-else:
+if len(l) == 0:
     print(-1)
+else:
+    print("{:.2f}".format(sum(set(l))/len(set(l))))
