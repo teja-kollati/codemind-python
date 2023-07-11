8a99674a4632c56749579437a208f85147c053db
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-07-11 18:41:34 +0530

    def count(List,n):     c = 0     for i in List:         if i == n:             c += 1     return c n = int(input()) List = list(map(int,input().split())) l = [] for i in List:     if count(List,i) == i:         l.append(i) if len(l) == 0:     print(-1) else:     print(min(l),max(l))

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -2,7 +2,7 @@ def count(List,n):
     c = 0
     for i in List:
         if i == n:
-            c += 1;
+            c += 1
     return c
 n = int(input())
 List = list(map(int,input().split()))
@@ -10,7 +10,7 @@ l = []
 for i in List:
     if count(List,i) == i:
         l.append(i)
-if len(l) != 0:
-    print(min(set(l)),max(set(l)))
-else:
+if len(l) == 0:
     print(-1)
+else:
+    print(min(l),max(l))
