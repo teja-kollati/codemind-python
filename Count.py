0775590c5278cb074e613c261ed9fedc7457e8db
teja kollati <119738827+teja-kollati@users.noreply.github.com>
2023-08-13 17:38:35 +0530

    n = int(input()) List = list(map(int,input().split())) even = 0 odd = 0 for i in range(0,len(List)):     if List[i]%2 == 0:         even += 1     else:         odd += 1 print(even,odd)

---
+++ Count.py
@@ -0,0 +1,10 @@
+n = int(input())
+List = list(map(int,input().split()))
+even = 0
+odd = 0
+for i in range(0,len(List)):
+    if List[i]%2 == 0:
+        even += 1
+    else:
+        odd += 1
+print(even,odd)
